4e134455079240c50ae812344c926569b1a792e8
akoolarni <arinpk210@gmail.com>
2023-10-30 14:55:36 +0530

    Added changesto the file

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -4,8 +4,8 @@ import { createUserWithEmailAndPassword , signInWithPopup , signOut} from "fireb
 
 export const Auth= ()=>{
 
-    const [email,setEmail]=useState("");
-    const [password ,setPassword]=useState("");
+    const [email,setEmail]=useState(" ");
+    const [password ,setPassword]=useState(" ");
 
     const handleSignIn=async ()=>{
         try{
