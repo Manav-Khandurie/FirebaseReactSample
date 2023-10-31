7d10c703d1d5b28fd8acb4862f65a97bd69c7fa3
dipenjava <dipenderkashyap158@gmail.com>
2023-10-31 11:14:36 +0530

    eight

---
+++ dipen/bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompt the user for their name
+echo "Please enter your name:"
+
+# Read the user's input and store it in a variable
+read username
+
+# Display a personalized greeting
+echo "Hello, $username! Welcome to the Bash scripting world!"
